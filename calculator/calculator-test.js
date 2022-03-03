548e0b1c157334ac80616a0d588256155dacc952
Alex <aengel38@gmail>
2022-03-02 18:00:54 -0800

    Test file updated with test cases

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,20 @@
-
-it('should calculate the monthly rate correctly', function () {
+it("should calculate the monthly rate correctly", function () {
   // ...
+  const values = {
+    amount: 30000,
+    years: 7,
+    rate: 4.5,
+  };
+  expect(calculateMonthlyPayment(values)).toEqual("11250.00");
 });
 
-
-it("should return a result with 2 decimal places", function() {
-  // ..
+it("should return a result with 2 decimal places", function () {
+  // ..Meant to fail intentionally
+  const values = {
+    amount: 30000,
+    years: 7,
+    rate: 4.5,
+  };
+  expect(calculateMonthlyPayment(values)).toEqual("11250.0");
+  expect(calculateMonthlyPayment(values)).toEqual("11250.08888");
 });
-
-/// etc
